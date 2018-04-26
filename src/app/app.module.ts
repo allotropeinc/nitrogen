4ba66e1f60c69b32a8ec610d4522f6df605750b7
Logan Darklock <logandarklock@gmail.com>
2018-04-26 16:33:19 -0700

    Materialize!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { MaterialModule } from './material/material.module';
 
 
 import { AppComponent } from './app.component';
@@ -12,7 +13,8 @@ import { AppComponent } from './app.component';
 	],
 	imports: [
 		BrowserModule,
-		BrowserAnimationsModule
+		BrowserAnimationsModule,
+		MaterialModule
 	],
 	providers: [],
 	bootstrap: [AppComponent]
