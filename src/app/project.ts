f1f5be37f73d682547fadd56afd8d2142570f4cd
Logan Darklock <logandarklock@gmail.com>
2018-06-06 16:38:11 -0700

    Fix error in project.ts

--- src/app/project.ts
+++ src/app/project.ts
@@ -1,6 +1,11 @@
 export interface Project {
-	id : number,
-	name : string,
-	code : string,
+	id : number
+	name : string
+	code : string
 	publishToken? : string
+	/**
+	 * 0: HTML
+	 * 1: Markdown
+	 */
+	type : number
 }
