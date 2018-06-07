949274802015849b3291f6fd576cf7eb9a8d3e5c
Logan Darklock <logandarklock@gmail.com>
2018-06-06 22:19:45 -0700

    Add version to footer, and add more footer

--- src/app/global-footer/global-footer.component.ts
+++ src/app/global-footer/global-footer.component.ts
@@ -1,5 +1,7 @@
 import { Component, isDevMode, OnInit } from '@angular/core'
 
+declare var require : any
+
 @Component ( {
 	selector    : 'app-global-footer',
 	templateUrl : './global-footer.component.html',
@@ -7,10 +9,11 @@ import { Component, isDevMode, OnInit } from '@angular/core'
 } )
 export class GlobalFooterComponent implements OnInit {
 	isDev = isDevMode ()
+	version : string
 
 	constructor () {}
 
 	ngOnInit () {
+		this.version = require ( '../../../package.json' ).version
 	}
-
 }
