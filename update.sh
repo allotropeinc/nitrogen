4a379e431e5d0a7399946f4de87b479f62f3021b
Logan Darklock <logandarklock@gmail.com>
2018-06-12 11:48:12 -0700

    Update backend before app

--- update.sh
+++ update.sh
@@ -2,14 +2,11 @@
 
 set -e
 
+git reset --hard
+git pull origin backend
+npm i
 cd ..
 git reset --hard
 git pull origin master
-git reset --hard
 npm i
 bash build.sh
-cd backend
-git reset --hard
-git pull origin backend
-git reset --hard
-npm i
