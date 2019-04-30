000000579a9f650bc6295168150489bdee1de381
Logan Darklock <logandark@allotropelabs.com>
2019-04-30 10:21:33 -0700

    Amended build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ set -e
 
 rm -rf backend/app-temp
 mkdir backend/app-temp
-ng build --prod --aot --output-path backend/app-temp/
+./node_modules/.bin/ng build --prod --aot --output-path backend/app-temp/
 rm -rf backend/app
 mv backend/app-temp backend/app
